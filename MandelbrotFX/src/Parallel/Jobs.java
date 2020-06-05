9717a95bb1b3cdfe2bf320775b0211c180a38464
jwar663 <32050015+jwar663@users.noreply.github.com>
2020-06-06 00:15:36 +1200

    Has visualisation per chunk
    need to update to consider which thread is doing each part rather than the number of the chunk

--- MandelbrotFX/src/Parallel/Jobs.java
+++ MandelbrotFX/src/Parallel/Jobs.java
@@ -28,10 +28,69 @@ public class Jobs implements Runnable{
                 return "#000000";
             case 1:
                 return "#2F4F4F";
-            case 3:
+            case 2:
                 return "#708090";
+            case 3:
+                return "#696969";
+            case 4:
+                return "#800000";
+            case 5:
+                return "#A0522D";
+            case 6:
+                return "#808000";
+            case 7:
+                return "#DAA520";
+            case 8:
+                return "#BC8F8F";
+            case 9:
+                return "#FFDEAD";
+            case 10:
+                return "#000080";
+            case 11:
+                return "#4169E1";
+            case 12:
+                return "#ADD8E6";
+            case 13:
+                return "#00FFFF";
+            case 14:
+                return "#556B2F";
+            case 15:
+                return "#9ACD32";
+            case 16:
+                return "#98FB98";
+            case 17:
+                return "#FFFF00";
+            case 18:
+                return "#FF8C00";
+            case 19:
+                return "#DC143C";
+            case 20:
+                return "#CD5C5C";
+            case 21:
+                return "#4B0082";
+            case 22:
+                return "#483D8B";
+            case 23:
+                return "#8B008B";
+            case 24:
+                return "#BA55D3";
+            case 25:
+                return "#D8BFD8";
+            case 26:
+                return "#FF69B4";
+            case 27:
+                return "#FFC0CB";
+            case 28:
+                return "#BDB76B";
+            case 29:
+                return "#7CFC00";
+            case 30:
+                return "#B0C4DE";
+            case 31:
+                return "#FAEBD7";
         }
-        return "lol";
+        //unreachable
+        return "#ffffff";
     }
 
     @Override
